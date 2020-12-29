2abe0a61b149cafd987dffacf1d85c9645c1706b
Caocp <2464027484@qq.com>
2020-12-29 21:25:19 +0800

    用户管理部门选择

--- src/api/service.js
+++ src/api/service.js
@@ -3,7 +3,7 @@ import request from '@/utils/request'
 //用户管理部门列表数据
 export function deptList(pid) {
     return request({
-        url: `/api/dept?sort=id,desc&${pid}`,
+        url: `/api/dept?sort=id,desc&pid=${pid}`,
         method: 'get'
     })
 }
--- src/views/system/user/index.vue
+++ src/views/system/user/index.vue
@@ -1,108 +1,111 @@
 <template>
-    <div>
-        <page-header-wrapper></page-header-wrapper>
-        <div style="margin-top: 40px;">
-            <a-row>
-                <a-col :span='4'><a-input-search placeholder="输入部门名称搜索" style="width: 80%" /></a-col>
-                <a-col :span='4'><a-input placeholder="输入名称或者邮箱搜索" style="width: 80%" /></a-col>
-                <a-col :span='4'><a-range-picker @change="onChange" /></a-col>
-                <a-col :span='4'>
-                    <a-select
-                        show-search
-                        placeholder="状态"
-                        option-filter-prop="children"
-                        style="width: 80%;margin-left: 40px;"
-                        @change="handleChange"
-                    >
-                        <a-select-option value='1'>激活</a-select-option>
-                        <a-select-option value='2'>锁定</a-select-option>
-                    </a-select>
-                </a-col>
-                <a-col :span='8' style="padding-left: 2%;">
-                    <a-button icon="search" type="primary">
-                        Search
-                    </a-button>
-                    <a-button icon="reset" type="primary" style="margin-left: 2%;">
-                        重置
-                    </a-button>
-                </a-col>    
-            </a-row>
-            <a-row>
-                <a-col :span='4'>
-                    <a-tree :load-data="onLoadData" :tree-data="treeData" />
-                </a-col>
-                <a-col :span='20'>
-
-                </a-col>
-            </a-row>
-
-        </div>
+  <div>
+    <page-header-wrapper></page-header-wrapper>
+    <div style="margin-top: 40px;">
+      <a-row>
+        <a-col :span="4"><a-input-search placeholder="输入部门名称搜索" style="width: 80%"/></a-col>
+        <a-col :span="4"><a-input placeholder="输入名称或者邮箱搜索" style="width: 80%"/></a-col>
+        <a-col :span="4"><a-range-picker @change="onChange"/></a-col>
+        <a-col :span="4">
+          <a-select
+            show-search
+            placeholder="状态"
+            option-filter-prop="children"
+            style="width: 80%;margin-left: 40px;"
+            @change="handleChange"
+          >
+            <a-select-option value="1">激活</a-select-option>
+            <a-select-option value="2">锁定</a-select-option>
+          </a-select>
+        </a-col>
+        <a-col :span="8" style="padding-left: 2%;">
+          <a-button icon="search" type="primary">
+            Search
+          </a-button>
+          <a-button icon="reset" type="primary" style="margin-left: 2%;">
+            重置
+          </a-button>
+        </a-col>
+      </a-row>
+      <a-row>
+        <a-col :span="4">
+          <a-tree :load-data="onLoadData" :tree-data="treeData" />
+        </a-col>
+        <a-col :span="20"> </a-col>
+      </a-row>
     </div>
+  </div>
 </template>
 
 <script>
 import { deptList } from '../../../api/service'
 export default {
-    data(){
-        return {
-            expandedKeys: [],
-            searchValue: '',
-            autoExpandParent: true,
-            treeData: [],
-            pid:''
-        }
-    },
-    created(){
-        this.getDeptList(this.pid)
-    },
-    methods: {
-        onChange(date, dateString) {//日期选择
-            console.log(date, dateString);
-        },
-        handleChange(value) {//状态选择
-            console.log(`selected ${value}`);
-        },
-         onExpand(expandedKeys) {
-            this.expandedKeys = expandedKeys;
-            this.autoExpandParent = false;
-          },
-          changeTree(selectedKeys){
-              this.pid = selectedKeys
-              this.getDeptList(selectedKeys.id)
-          },
-
-        onLoadData(treeNode) {
-            return new Promise(resolve => {
-                console.log(treeNode.dataRef)
-                if (treeNode.dataRef.children) {
-                resolve();
-                return;
-                }
-                treeNode.dataRef.children = [
-                { title: 'Child Node', key: `${treeNode.eventKey}-0` },
-                { title: 'Child Node', key: `${treeNode.eventKey}-1` },
-                ];
-                this.treeData = [...this.treeData];
-                resolve();
-            });
-            },
-        async getDeptList(pid){
-            const { content } = await deptList(pid)
-            let customColumns = [];
-            content.map((item,index) => {
-                var obj = {
-                    title: item.name,
-                    id: item.id,
-                    isLeaf: item.leaf
-                }
-                customColumns.push(obj)
-            })
-            this.treeData = customColumns
-        }
+  data() {
+    return {
+      expandedKeys: [],
+      searchValue: '',
+      autoExpandParent: true,
+      treeData: [],
+      pid: ''
     }
+  },
+  created() {
+    this.getDeptList(this.pid)
+  },
+  methods: {
+    onChange(date, dateString) {
+      //日期选择
+      console.log(date, dateString)
+    },
+    handleChange(value) {
+      //状态选择
+      console.log(`selected ${value}`)
+    },
+    onExpand(expandedKeys) {
+      this.expandedKeys = expandedKeys
+      this.autoExpandParent = false
+    },
+    changeTree(selectedKeys) {
+      this.pid = selectedKeys
+      this.getDeptList(selectedKeys.id)
+    },
+
+    onLoadData(treeNode) {
+      return new Promise(resolve => {
+        if (treeNode.dataRef.children) {
+          resolve()
+          return
+        }
+        const id = treeNode.dataRef.key;
+        const result = deptList(id).then((res) => {
+            const children = res.content.map(item => {
+                return {
+                    title: item.name,
+                    key: item.id,
+                }
+            })
+            treeNode.dataRef.children = children
+            this.treeData = [...this.treeData]
+            resolve()
+        });
+
+      })
+    },
+    async getDeptList(pid) {
+      const { content } = await deptList(pid)
+      let customColumns = []
+      content.map((item, index) => {
+        var obj = {
+          title: item.name,
+          key: item.id,
+          isLeaf: item.leaf
+        }
+        customColumns.push(obj)
+      })
+      this.treeData = customColumns
+    }
+  }
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
